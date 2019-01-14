c815db5e8e2f1ceac4275fbd5118a79fc242451d
Adnan-Hussain22 <adnanrajput42@gmail.com>
2019-01-14 13:45:50 +0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/GH-Pages-Deploy/precache-manifest.a629ab7d5a84befa6aa3f18f9308a778.js"
+  "/precache-manifest.07d435d88da87eccb1828383eab8158e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/GH-Pages-Deploy/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
